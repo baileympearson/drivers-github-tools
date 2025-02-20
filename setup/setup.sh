65470eb8c5b9c0069c88ad4f57f2744dc418cd33
bailey <bailey.pearson@mongodb.com>
2025-02-20 10:20:40 -0700

    try new token?

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,8 +6,8 @@ prefix=$(echo $AWS_SECRET_ID | tr '[:lower:]' '[:upper:]' | sed -r 's/[-/]+/_/g'
 prefix=${prefix}_
 vars=$(compgen -A variable | grep $prefix | tr '\n' ' ')
 for var in $vars; do
-     new_key=$(echo $var | sed "s/$prefix//g")
-     declare $new_key=${!var}
+  new_key=$(echo $var | sed "s/$prefix//g")
+  declare $new_key=${!var}
 done
 
 echo "::group::Set up artifactory"
@@ -20,7 +20,7 @@ echo "::endgroup::"
 
 echo "Set up envfile for garasign"
 GARASIGN_ENVFILE=/tmp/garasign-envfile
-cat << EOF > $GARASIGN_ENVFILE
+cat <<EOF >$GARASIGN_ENVFILE
 GRS_CONFIG_USER1_USERNAME=$GARASIGN_USERNAME
 GRS_CONFIG_USER1_PASSWORD=$GARASIGN_PASSWORD
 EOF
@@ -28,29 +28,29 @@ EOF
 if [ -n "${SILKBOMB_USER:-}" ]; then
   echo "Set up envfile for silkbomb"
   SILKBOMB_ENVFILE=/tmp/silkbomb-envfile
-  cat << EOF > $SILKBOMB_ENVFILE
+  cat <<EOF >$SILKBOMB_ENVFILE
 SILK_CLIENT_ID=${SILKBOMB_USER}
 SILK_CLIENT_SECRET=${SILKBOMB_KEY}
 EOF
 fi
 
-if [ -n "${KONDUKTO_TOKEN:-}" ]; then
+if [ -n "${DRIVERS_RELEASE_NODE_KONDUKTO_TOKEN:-}" ]; then
   echo "Setting up envfile for Kondukto"
   KONDUKTO_ENVFILE=/tmp/kondukto-envfile
-  cat << EOF > $KONDUKTO_ENVFILE
-KONDUKTO_TOKEN=${KONDUKTO_TOKEN}
+  cat <<EOF >$KONDUKTO_ENVFILE
+KONDUKTO_TOKEN=${DRIVERS_RELEASE_NODE_KONDUKTO_TOKEN}
 EOF
 fi
 
 echo "Set up output directories"
 export RELEASE_ASSETS=/tmp/release-assets
 mkdir $RELEASE_ASSETS
-echo "$GITHUB_RUN_ID" > $RELEASE_ASSETS/release_run_id.txt
+echo "$GITHUB_RUN_ID" >$RELEASE_ASSETS/release_run_id.txt
 export S3_ASSETS=/tmp/s3-assets
 mkdir $S3_ASSETS
 
 echo "Set up global variables"
-cat <<EOF >> $GITHUB_ENV
+cat <<EOF >>$GITHUB_ENV
 AWS_BUCKET=${RELEASE_ASSETS_BUCKET:-}
 GPG_KEY_ID=$GPG_KEY_ID
 GPG_PUBLIC_URL=${GPG_PUBLIC_URL:-}
